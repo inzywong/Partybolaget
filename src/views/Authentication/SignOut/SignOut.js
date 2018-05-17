e8ef97706bab6b36df75ebdb9f7f5f912c02a78e
InzyWong <vwong@kth.se>
2018-05-17 16:46:42 +0200

    logout

--- src/views/Authentication/SignOut/SignOut.js
+++ src/views/Authentication/SignOut/SignOut.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import fire from '../../../firebase/firebase';
+import { Redirect } from 'react-router';
+
 
 class SignOut extends Component {
-    constructor(props) {
-      super(props);
-      this.signOut = this.signOut.bind(this);
+  constructor(props) {
+    super(props);
+    this.signOut = this.signOut.bind(this);
+    this.state = {
+      toWelcome: false,
     }
+  }
 
-    signOut(){
-        fire.auth().signOut();
-    }
-    render() {
-        return (
-          <button type="submit" onClick={this.signOut} class="btn btn-primary">Logout</button>
-        );
-      }
+  signOut() {
+    fire.auth().signOut();
+    this.setState({
+      toWelcome: true
+    })
+  }
+  render() {
+    if(this.state.toWelcome)
+  		{
+  				return <Redirect push to="/welcome" />;
+  		}
+    return (
+      <button type="submit" onClick={this.signOut} class="btn btn-primary">Logout</button>
+    );
+  }
 }
 
 export default SignOut;
