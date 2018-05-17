1b4fbfb27b0f285a55f6b0a9a4eeda0cf659908d
satucl <satucahaya.langit@gmail.com>
2018-05-18 01:28:52 +0200

    css search drink
    yes

--- src/views/FilterDrink/FilterDrink.js
+++ src/views/FilterDrink/FilterDrink.js
@@ -18,7 +18,7 @@ class FilterDrink extends Component {
   }
 
   componentDidMount = () => {
-      // Get the name of the first drink type on the menu so that we can highlight it.
+    // Get the name of the first drink type on the menu so that we can highlight it.
     this.props.model.addObserver(this);
   }
 
@@ -49,59 +49,65 @@ class FilterDrink extends Component {
   }
 
   onSortClicked = (e) => {
-      if (this.props.model.getSortStatus()=="DESC"){
-        this.props.model.setSortBy("ASC");
-        this.state.message = <p>&#8595; Ascending</p>;
-      } else if(this.props.model.getSortStatus()=="ASC"){
-        this.props.model.setSortBy("DESC");
-        this.state.message = <p>&#8593; Descending</p>;
-      }
+    if (this.props.model.getSortStatus()=="DESC"){
+      this.props.model.setSortBy("ASC");
+      this.state.message = <p>&#8595; Ascending</p>;
+    } else if(this.props.model.getSortStatus()=="ASC"){
+      this.props.model.setSortBy("DESC");
+      this.state.message = <p>&#8593; Descending</p>;
+    }
   }
 
   render() {
     return (
       <div className="FilterDrink row">
-        <h4>{this.state.filterDrink}</h4>
+
         <div className="partyDetail row">
-          <p className="col">{this.state.partyName}</p>
-          <p className="col">{this.state.numberOfGuests}</p>
-          <p className="col">{this.state.partyDuration}</p>
+          <div className="col-sm-4">{this.state.partyName}</div>
+          <div className="col-sm-4">{this.state.numberOfGuests}</div>
+          <div className="col-sm-4">{this.state.partyDuration}</div>
         </div>
         <div className="drinkPrice row">
-          <p>{this.state.drinkPrice}</p>
-          <label>
-            <input type="number" onChange={this.onMinPriceTextChanged} placeholder="Minimal Price"/>kr
-          </label>
-          <label>
-            <input type="number" onChange={this.onMaxPriceTextChanged} placeholder="Maximal Price"/>kr
-          </label>
-        </div>
-        <div className="alcoholPercentage row">
-          <p>{this.state.alcoholPercentage}</p>
-          <label>
-            <input type="number" onChange={this.onMinAlcoholTextChanged} placeholder="Minimal Alcohol Percentage"/> %
-          </label>
-          <label>
-            <input type="number" onChange={this.onMaxAlcoholTextChanged} placeholder="Maximal Alcohol Percentage"/> %
-          </label>
-        </div>
-        <div className="row">
-          <p>{this.state.sort}</p>
-          <select className="col-sm-2" onChange={this.onSearchTypeChanged}>
-            <option value="">All</option>
-            <option value="name">Name</option>
-            <option value="price">Price</option>
-            <option value="alcohol">Alcohol Percentage</option>
-          </select>
-          <div>
-            <button value={this.state.sortStatus} onClick={this.onSortClicked} className="btn btn-danger">
-              {this.state.message}
-            </button>
+          <div className="col-sm-4"><p>{this.state.drinkPrice} (SEK)  </p>
+            <label><input type="number" onChange={this.onMinPriceTextChanged} placeholder="Minimal" /></label>
+            <label><input type="number" onChange={this.onMaxPriceTextChanged} placeholder="Maximal" /></label>
+
+          </div>
+          <div className="col-sm-4"><p>{this.state.alcoholPercentage} (%)</p>
+            <label>
+              <input type="number" onChange={this.onMinAlcoholTextChanged} placeholder="Minimal"/>
+            </label>
+            <label>
+              <input type="number" onChange={this.onMaxAlcoholTextChanged} placeholder="Maximal"/>
+            </label>
+          </div>
+          <div className="col-sm-4">
+            <p>{this.state.sort}</p>
+            <div>
+              <select   onChange={this.onSearchTypeChanged}>
+                <option value="">All</option>
+                <option value="name">Name</option>
+                <option value="price">Price</option>
+                <option value="alcohol">Alcohol Percentage</option>
+              </select>
+            </div>
+            <div>
+              <button value={this.state.sortStatus} onClick={this.onSortClicked} className="btn btn-danger">
+                {this.state.message}
+              </button>
+            </div>
+
+          </div>
+
+
+          </div>
+
+          <div className="row">
+
           </div>
         </div>
-      </div>
-    );
-  }
-}
+        );
+        }
+        }
 
-export default FilterDrink;
+        export default FilterDrink;
